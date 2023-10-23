9dcb686c20666ad09eb05c84b19745f9e92a251e
Lukas Carvajal <lukascarvajal@gmail.com>
2023-10-23 11:14:49 +0200

    Initial Commit

---
+++ JokingPanda/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JokingPanda
+//
+//  Created by Lukas Carvajal on 10/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JokingPanda/JokingPandaApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokingPandaApp.swift
+//  JokingPanda
+//
+//  Created by Lukas Carvajal on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct JokingPandaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
