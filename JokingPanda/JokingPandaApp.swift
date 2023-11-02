b8926d660369a08efd29b93ef93d72b28efe2112
Lukas Carvajal <lukascarvajal@gmail.com>
2023-11-02 15:10:30 +0100

    Hide status bar for app

--- JokingPanda/JokingPandaApp.swift
+++ JokingPanda/JokingPandaApp.swift
@@ -13,7 +13,7 @@ struct JokingPandaApp: App {
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().statusBar(hidden: true)
         }
     }
 }
