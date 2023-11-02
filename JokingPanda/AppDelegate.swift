c5bb1f9916029303d65f3997d0fcb89effbddf92
Lukas Carvajal <lukascarvajal@gmail.com>
2023-11-02 11:38:17 +0100

    Add Mixpanel package

---
+++ JokingPanda/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  JokingPanda
+//
+//  Created by Lukas Carvajal on 11/2/23.
+//
+
+import Foundation
+import UIKit
+import Mixpanel
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+//        Mixpanel.initialize(token: "Your Token", trackAutomaticEvents: true)
+//        Mixpanel.mainInstance().track(event: "Signed Up", properties: [
+//            "Signup Type": "Referral",
+//        ])
+        return true
+    }
+}
--- JokingPanda/JokingPandaApp.swift
+++ JokingPanda/JokingPandaApp.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 @main
 struct JokingPandaApp: App {
+    @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
