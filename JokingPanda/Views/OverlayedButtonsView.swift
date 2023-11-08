f9b5dc1bc31c4dd2a3df19eef71abf33df114e9e
Lukas Carvajal <lukascarvajal@gmail.com>
2023-11-08 22:25:57 +0100

    Move two smaller views which have to do with the overlayed buttons into same file

--- JokingPanda/Views/OverlayedButtonsView.swift
+++ JokingPanda/Views/OverlayedButtonsView.swift
@@ -26,6 +26,49 @@ struct OverlayedButtonsView: View {
     }
 }
 
+struct PulsingFingerTapView: View {
+    internal let size: CGFloat
+    
+    var body: some View {
+        if #available(iOS 17.0, *) {
+            Image(systemName: "hand.tap.fill")
+                .symbolRenderingMode(.palette)
+                .font(.system(size: size))
+                .foregroundStyle(.white, .tappableAccent)
+                .symbolEffect(.pulse, options: .repeating, isActive: true)
+                .padding()
+            
+        } else {
+            Image(systemName: "hand.tap.fill")
+                .font(.system(size: size))
+                .foregroundStyle(.white, .tappableAccent)
+                .padding()
+        }
+    }
+}
+
+struct SettingsButtonView: View {
+    @Binding var showSheet: Bool
+    
+    var body: some View {
+        Button(action: {
+            self.showSheet.toggle()
+        }) {
+            Label("", systemImage: "gear")
+                .symbolRenderingMode(.palette)
+                .font(.system(size: 20))
+                .foregroundStyle(.tappableAccent)
+                .padding()
+                .padding(.top, 10)
+        }
+        .sheet(isPresented: $showSheet) {
+            NavigationView {
+                SettingsView()
+            }
+        }
+    }
+}
+
 #Preview {
     OverlayedButtonsView()
 }
