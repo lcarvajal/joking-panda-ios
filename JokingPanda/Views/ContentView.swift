2f1dbd98f2fe177f1396e02564cf1f562b65618c
Lukas Carvajal <lukascarvajal@gmail.com>
2023-11-08 23:40:51 +0100

    Fix bug causing content view not to update once authorization permitted for mic and speech recognition

--- JokingPanda/Views/ContentView.swift
+++ JokingPanda/Views/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
     
     var body: some View {
         if microphoneStatus != .authorized || speechStatus != .authorized {
-            AuthorizationsView()
+            AuthorizationsView(speechStatus: $speechStatus, microphoneStatus: $microphoneStatus)
         }
         else {
             BotInterfaceView()
