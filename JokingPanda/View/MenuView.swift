98306cc2d6eb78218c4aa4b401c967cd7b395e15
Lukas Carvajal <lukascarvajal@gmail.com>
2023-11-20 23:26:33 +0100

    Create main menu screen to allow navigation to different parts of the app

---
+++ JokingPanda/View/MenuView.swift
@@ -0,0 +1,69 @@
+//
+//  AuthorizationView.swift
+//  JokingPanda
+//
+//  Created by Lukas Carvajal on 10/27/23.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    @State var showSheet = false
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                GeometryReader { geometry in
+                    ZStack {
+                        AnimationView(geometry: .constant(geometry), status: Binding.constant(ConversationStatus.noOneSpeaking))
+                        VStack {
+                            HStack {
+                                Spacer()
+                                SettingsButtonView(showSheet: $showSheet)
+                            }
+                            Spacer()
+                            HStack {
+                                Spacer()
+                                NavigationLink(destination: BotInterfaceView()) {
+                                    Image(systemName: "book.closed.fill") // System icon
+                                                                .font(.system(size: 36))
+                                                                .foregroundColor(.tappableAccent)
+                                                                .symbolRenderingMode(.palette)
+                                }
+                                Spacer()
+                                NavigationLink(destination: BotInterfaceView()) {
+                                    Image(systemName: "music.quarternote.3") // System icon
+                                                                .font(.system(size: 36))
+                                                                .foregroundColor(.tappableAccent)
+                                                                .symbolRenderingMode(.palette)
+                                }
+                                Spacer()
+                                NavigationLink(destination: BotInterfaceView()) {
+                                    Image(systemName: "figure.socialdance") // System icon
+                                                                .font(.system(size: 36))
+                                                                .foregroundColor(.tappableAccent)
+                                                                .symbolRenderingMode(.palette)
+                                }
+                                Spacer()
+                                NavigationLink(destination: BotInterfaceView()) {
+                                    Image(systemName: "face.smiling.fill") // System icon
+                                                                .font(.system(size: 36))
+                                                                .foregroundColor(.tappableAccent)
+                                                                .symbolRenderingMode(.palette)
+                                }
+                                Spacer()
+                            }
+                            .padding()
+                            .padding(.bottom, 10)
+                        }
+                    }
+                }
+            }
+            .background(Color.background)
+        }
+    }
+}
+
+#Preview {
+    MenuView()
+}
