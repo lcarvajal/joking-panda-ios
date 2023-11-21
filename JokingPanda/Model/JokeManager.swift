460d499c0e77209c80520811eea93c2a6c285754
Lukas Carvajal <lukascarvajal@gmail.com>
2023-11-21 10:37:43 +0100

    Extract logic for jokes from ConversationManager into JokeManager

---
+++ JokingPanda/Model/JokeManager.swift
@@ -0,0 +1,42 @@
+//
+//  JokeManager.swift
+//  JokingPanda
+//
+//  Created by Lukas Carvajal on 11/21/23.
+//
+
+import Foundation
+
+struct JokeManager {
+    internal var currentJoke: Conversation {
+        return knockKnockJokes[index]
+    }
+    
+    private var index = 0
+    private let knockKnockJokes: [Conversation] = Tool.load("knockKnockJokeData.json")
+    
+    // MARK: - Setup
+    
+    init() {
+        setIndexFromLastJokeHeard()
+    }
+    
+    private mutating func setIndexFromLastJokeHeard() {
+        let id = UserDefaults.standard.integer(forKey: Constant.UserDefault.conversationId)
+        if let index = knockKnockJokes.firstIndex(where: { $0.id == id }) {
+            self.index = index
+        }
+    }
+    
+    // MARK: - Actions
+    
+    internal mutating func currentJokeWasHeard() {
+        index += 1
+        
+        if index > (knockKnockJokes.count - 1) {
+            index = 0
+        }
+        
+        UserDefaults.standard.set(knockKnockJokes[index].id, forKey: Constant.UserDefault.conversationId)
+    }
+}
