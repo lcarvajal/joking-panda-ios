5587c2029af2eaf12d5cb8929ecea3599cf869ac
Lukas Carvajal <lukascarvajal@gmail.com>
2024-06-04 16:39:53 +0200

    Update audio playback errors with localized error descriptions

--- JokingPanda/Features/Bot/Model/AudioAndSpeech/AudioPlayer.swift
+++ JokingPanda/Features/Bot/Model/AudioAndSpeech/AudioPlayer.swift
@@ -8,6 +8,24 @@ import AVFAudio
 
 protocol AudioPlayerDelegate: AnyObject {
     func didPlay()
+    func errorDidOccur(error: Error)
+}
+
+enum AudioPlayerError: LocalizedError {
+    case audioPlaybackFailed
+    case playerSetupFailed
+    case sessionSetupFailed
+    
+    var errorDescription: String? {
+        switch self {
+        case .audioPlaybackFailed:
+            return "Could not play audio."
+        case .playerSetupFailed:
+            return "Audio player setup failed."
+        case .sessionSetupFailed:
+            return "Audio session setup failed."
+        }
+    }
 }
 
 class AudioPlayer: NSObject {
@@ -15,36 +33,56 @@ class AudioPlayer: NSObject {
     
     private var audioPlayer: AVAudioPlayer? = nil
     
-    internal func start(url: URL) throws {
+    internal func start(url: URL) {
         if audioPlayer == nil {
-            try setUpAudioSession()
-            try setUpAudioPlayer(url: url)
-            startAudioPlayer()
+            do {
+                try setUpAudioSession()
+                try setUpAudioPlayer(url: url)
+                try startAudioPlayer()
+            }
+            catch {
+                delegate?.errorDidOccur(error: error)
+            }
+        }
+        else {
+            debugPrint("Attempting to start audio player after it's already been started.")
         }
     }
     
-    internal func stop() throws {
+    internal func stop() {
         audioPlayer?.delegate = nil
         audioPlayer = nil
-        try AVAudioSession.sharedInstance().setActive(false, options: .notifyOthersOnDeactivation)
+        try? AVAudioSession.sharedInstance().setActive(false, options: .notifyOthersOnDeactivation)
     }
     
     // MARK: - Set up
     
     private func setUpAudioSession() throws {
-        try AVAudioSession.sharedInstance().setCategory(.playAndRecord, mode: .default, options: [.defaultToSpeaker])
-        try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playAndRecord, mode: .default, options: [.defaultToSpeaker])
+            try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
+        }
+        catch {
+            throw AudioPlayerError.sessionSetupFailed
+        }
     }
     
     private func setUpAudioPlayer(url: URL) throws {
-        audioPlayer = try AVAudioPlayer(contentsOf: url)
-        audioPlayer?.delegate = self
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.delegate = self
+        }
+        catch {
+            throw AudioPlayerError.playerSetupFailed
+        }
     }
     
     // MARK: - Actions
     
-    private func startAudioPlayer() {
-        guard let audioPlayer = self.audioPlayer else { return }
+    private func startAudioPlayer() throws {
+        guard let audioPlayer = self.audioPlayer else {
+            throw AudioPlayerError.playerSetupFailed
+        }
         
         if !audioPlayer.isPlaying {
             audioPlayer.prepareToPlay()
@@ -55,8 +93,13 @@ class AudioPlayer: NSObject {
 
 extension AudioPlayer: AVAudioPlayerDelegate {
     internal func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
-        // FIXME: Handle successful and unsuccessful cases
-        try? stop()
-        delegate?.didPlay()
+        stop()
+        
+        if flag {
+            delegate?.didPlay()
+        } else {
+            delegate?.errorDidOccur(error: AudioPlayerError.audioPlaybackFailed)
+        }
+        
     }
 }
