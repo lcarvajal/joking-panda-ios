2c49fa4faf904fa57120d49f9fec245a4777beab
Lukas Carvajal <lukascarvajal@gmail.com>
2024-06-07 12:59:04 +0200

    Add test for valid and invalid urls

--- JokingPandaTests/AudioPlayerTests.swift
+++ JokingPandaTests/AudioPlayerTests.swift
@@ -27,25 +27,36 @@ final class AudioPlayerTests: XCTestCase {
     var mockDelegate: MockAudioPlayerDelegate!
     var testURL: URL!
     
-    override func setUp() {
-        super.setUp()
+    override func setUpWithError() throws {
+        try super.setUpWithError()
         audioPlayer = AudioPlayer()
         mockDelegate = MockAudioPlayerDelegate()
         audioPlayer.delegate = mockDelegate
-        testURL = Bundle.main.url(forResource: "knock-knock", withExtension: "m4a")
+        
+        guard let url = Bundle.main.url(forResource: "knock-knock", withExtension: "m4a") else {
+            throw NSError(domain: "Invalid URL", code: 1, userInfo: nil)
+        }
+        testURL = url
     }
-
-    override func tearDown() {
+    
+    override func tearDownWithError() throws {
         audioPlayer = nil
         mockDelegate = nil
         testURL = nil
-        super.tearDown()
+        try super.tearDownWithError()
     }
     
     func test_audioPlayer_withValidURL_shouldCallDidPlay() {
         audioPlayer.start(url: testURL)
-        audioPlayer.stop()
         XCTAssertTrue(mockDelegate.didPlay)
         XCTAssertNil(mockDelegate.error)
+    }
+    
+    func test_audioPlayer_withInvalidURL_shouldCallErrorDidOccur() {
+        let invalidURL = URL(fileURLWithPath: "/invalid/path")
+        audioPlayer.start(url: invalidURL)
+        XCTAssertFalse(mockDelegate.didPlay)
+        XCTAssertNotNil(mockDelegate.error)
+        XCTAssertEqual(mockDelegate.error as? AudioPlayerError, AudioPlayerError.playerSetupFailed)
     }
 }
