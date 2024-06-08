4781ca5f48fd442c58ccdf1c0a25ff0c239f90a4
Lukas Carvajal <lukascarvajal@gmail.com>
2024-06-08 19:37:29 +0200

    Remove logic for interpretting phrase

--- JokingPanda/Features/Bot/Model/Dialogue/PhraseHistory.swift
+++ JokingPanda/Features/Bot/Model/Dialogue/PhraseHistory.swift
@@ -15,25 +15,16 @@ class PhraseHistory {
      Appends a phrase to `history` in a newline.
      If the phrase is close to what is expected, the expected phrase gets appended instead of what was actually heard.
      */
-    internal func addPhrase(_ phraseSaidOrHeard: String, expectedPhrase: String?, saidBy personTalking: Person) {
-        let phrase: String
-        
-        if let expectedPhrase = expectedPhrase {
-            phrase = getInterpretedPhrase(phraseHeard: phraseSaidOrHeard, expectedPhrase: expectedPhrase)
-        }
-        else {
-            phrase = phraseSaidOrHeard
-        }
-        
+    internal func addPhrase(_ phraseSaidOrHeard: String, saidBy personTalking: Person) {
         if history != "" {
             history += "\n"
         }
         
         switch personTalking {
         case .bot:
-            history += "🐼 " + phrase
+            history += "🐼 " + phraseSaidOrHeard
         case .currentUser:
-            history += "🗣️ " + phrase
+            history += "🗣️ " + phraseSaidOrHeard
         }
     }
     
@@ -51,12 +42,5 @@ class PhraseHistory {
     
     internal func getHistory() -> String {
         return history
-    }
-    
-    /**
-     - returns: String interpreted phrase which returns the expected phrase when it closely matches the heard phrase.
-     */
-    private func getInterpretedPhrase(phraseHeard: String, expectedPhrase: String) -> String {
-        return Tool.levenshtein(aStr: phraseHeard, bStr: expectedPhrase) < 5 ? expectedPhrase : phraseHeard
     }
 }
