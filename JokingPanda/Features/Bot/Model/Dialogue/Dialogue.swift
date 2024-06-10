030b0a6eb557d8bcda63818d652ba96698555488
Lukas Carvajal <lukascarvajal@gmail.com>
2024-06-10 12:22:24 +0200

    Create initializer setting user and bot phrases

--- JokingPanda/Features/Bot/Model/Dialogue/Dialogue.swift
+++ JokingPanda/Features/Bot/Model/Dialogue/Dialogue.swift
@@ -9,6 +9,28 @@
 import Foundation
 
 struct Dialogue: Hashable, Codable, Identifiable {
-    var id: Int
-    var phrases: [String]
+    let id: Int
+    let phrases: [String]
+    let botPhrases: [String]
+    let userPhrases: [String]
+    
+    private enum CodingKeys: String, CodingKey {
+        case id, phrases
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(Int.self, forKey: .id)
+        
+        let phrases = try container.decode([String].self, forKey: .phrases)
+        self.phrases = phrases
+        self.botPhrases = stride(from: 0, to: phrases.count, by: 2).map { phrases[$0] }
+        self.userPhrases = stride(from: 1, to: phrases.count, by: 2).map { phrases[$0] }
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(phrases, forKey: .phrases)
+    }
 }
